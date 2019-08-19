9b47e7c371a0a81e27aeecb18e8686ddbe321aca
Matthew Ragas <s178023@students.aie.edu.au>
2019-08-19 16:07:52 -0500

    Add project files.

---
+++ LoopTutorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count = 0;
+
+            //while (count < 3)
+            //{
+            //    count++;
+            //    Console.WriteLine("The loop has run " + count + " times.");
+            //}
+            //Console.ReadKey();
+
+            for (int count = 0; count < 3; count++)
+            {
+                Console.WriteLine("The loop has run " + count + " times.");
+                Console.ReadKey();
+                if (count == 2)
+                {
+                    return;
+                }
+            }
+            Console.WriteLine("The loop is now finished.");
+            Console.ReadKey();
+        }
+    }
+}
